9c38a45ef162d6e223121d33e7516ba9b59ff7c1
AlphaHovi <50176948+AlphaHovi@users.noreply.github.com>
2019-05-11 19:07:36 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" type="text/css" href='style.css'>
+      
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+
+  <body>
+    <nav class='nav'>
+      <div class="nav-wrapper yellow darken-4">
+        <a href="index.html" class="brand-logo"><img src='newLogo.jpg'></a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="comeandplay.html">COME AND PLAY</a></li>
+          <li><a href="hire.html">HIRE</a></li>
+          <li><a href="about.html">ABOUT</a></li>
+          <li><a href="contact.html">CONTACT</a></li>
+          <li><a href="merch.html">MERCH</a></li>
+          <li><a href="login.html">LOGIN</a></li>
+        </ul>
+      </div>
+    </nav>
+  </body>
+</html>
